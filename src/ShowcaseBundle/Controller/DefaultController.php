e4c850356d5bb8e59ecbdd5df5b7917365f10d45
Sherbrow <git@sherbrow.fr>
2016-03-10 00:21:22 +0100

    Index page dump list

--- src/ShowcaseBundle/Controller/DefaultController.php
+++ src/ShowcaseBundle/Controller/DefaultController.php
@@ -8,6 +8,20 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ShowcaseBundle:Default:index.html.twig');
+        $router = $this->get('router');
+        $routes = $router->getRouteCollection();
+        
+        $appRoutes = [];
+        foreach ($routes as $routeName => $route) {
+            if(strpos($route->getPath(), '/_') !== false) continue;
+            $url = $router->generate($routeName, $route->getDefaults());
+            $appRoutes[] = $route;
+            $appUrls[] = $url;
+        }
+        
+        return $this->render('ShowcaseBundle:Default:index.html.twig', [
+            'routes' => $appRoutes,
+            'urls' => $appUrls,
+        ]);
     }
 }
